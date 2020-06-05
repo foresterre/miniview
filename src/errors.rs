cd14b482a415bdc2be9d2cf6b27f75ad581646cb
Martijn Gribnau <garm@ilumeo.com>
2020-06-05 22:21:09 +0200

    Update error handling
    Use thiserror and anyhow for convenience

--- src/errors.rs
+++ src/errors.rs
@@ -1,54 +1,37 @@
-use std::error::Error;
-use std::fmt::Display;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum MiniViewError {
+    #[error("No path to an image was provided.")]
     EmptyInputPath,
-    FailedToImport(ImportError),
+
+    #[error(transparent)]
+    FailedToImport(#[from] ImportError),
+
+    #[error("Unable to determine input mode.")]
     CliUnableToDetermineInputMode,
+
+    #[error("Unable to create a window to display the image.")]
     UnableToCreateWindow,
+
+    #[error("Unable to map the image to a texture.")]
     UnableToMapImage,
 }
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum ImportError {
+    #[error("Provided path to image could not be found or loaded.")]
     OnPathNotFound,
+
+    #[error("Unable to read from the stdin.")]
     OnStdinUnableToRead,
+
+    #[error("Given path to image was empty.")]
     OnStdinPathWasEmpty,
+
+    #[error("Stdin was empty.")]
     OnStdinBytesStreamWasEmpty,
+
+    #[error("The input received from stdin could not be loaded.")]
     OnStdinBytesUnableToGuessOrLoadFormat,
-}
-
-impl Display for MiniViewError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", self.description())
-    }
-}
-
-impl Error for MiniViewError {
-    fn description(&self) -> &str {
-        match *self {
-            MiniViewError::EmptyInputPath => "No path to an image was provided.",
-            MiniViewError::FailedToImport(ImportError::OnPathNotFound) => {
-                "Provided path to image could not be found or loaded."
-            }
-            MiniViewError::FailedToImport(ImportError::OnStdinUnableToRead) => {
-                "Unable to read from the stdin."
-            }
-            MiniViewError::FailedToImport(ImportError::OnStdinPathWasEmpty) => {
-                "Given path to image was empty."
-            }
-            MiniViewError::FailedToImport(ImportError::OnStdinBytesStreamWasEmpty) => {
-                "Stdin was empty."
-            }
-            MiniViewError::FailedToImport(ImportError::OnStdinBytesUnableToGuessOrLoadFormat) => {
-                "The input received from stdin could not be loaded."
-            }
-            MiniViewError::CliUnableToDetermineInputMode => "Unable to determine input mode.",
-            MiniViewError::UnableToCreateWindow => {
-                "Unable to create a window to display the image."
-            }
-            MiniViewError::UnableToMapImage => "Unable to map the image to a texture.",
-        }
-    }
 }
